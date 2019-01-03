5be306c2e75537c926b573e77120eecac023d06d
honghuachen <chengdudahonghua@outlook.com>
2019-01-03 22:00:59 +0800

    修改build文件位置

---
+++ publish/ios/tools/XcodeProjectSetting/build.bat
@@ -0,0 +1,6 @@
+@echo off
+set BIN=./XcodeSetting.exe
+set CC="D:\Program Files\Unity2017.4.15f1\Editor\Data\MonoBleedingEdge\lib\mono\4.5\mcs.exe"
+
+%CC% XcodeSetting/*.cs XcodeSetting/PBX/*.cs XcodeSetting/XcodeSetting/*.cs -out:%BIN% -r:./XcodeSetting/bin/System.Xml.Linq.dll
+pause
--- publish/ios/tools/XcodeProjectSetting/build.sh
+++ publish/ios/tools/XcodeProjectSetting/build.sh
@@ -1,5 +1,5 @@
 BIN=./bin/XcodeSetting.exe
 MCS="D:\Program Files\Unity2017.4.15f1\Editor\Data\MonoBleedingEdge\lib\mono\4.5\mcs.exe"
 
-"$MCS" *.cs PBX/*.cs XcodeSetting/*.cs -out:$BIN -debug -unsafe -r:./bin/System.Xml.Linq.dll
+"$MCS" XcodeSetting/*.cs XcodeSetting/PBX/*.cs XcodeSetting/XcodeSetting/*.cs -out:$BIN -debug -unsafe -r:./XcodeSetting/bin/System.Xml.Linq.dll
 read -p "按回车键继续"
