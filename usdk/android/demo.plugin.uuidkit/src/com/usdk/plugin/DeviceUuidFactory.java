ce55a9c6fdbeae175c4cbf6eeadc97a2dea43251
chengdudahonghua <chengdudahonghua@outlook.com>
2020-05-27 14:06:23 +0800

    增加uuid插件获取设备uuid

---
+++ usdk/android/demo.plugin.uuidkit/src/com/usdk/plugin/DeviceUuidFactory.java
@@ -0,0 +1,117 @@
+package com.usdk.plugin;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Environment;
+import android.provider.Settings;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+public class DeviceUuidFactory {
+    protected final String PREFS_FILE            = "dev_id.xml";
+    protected final String DEVICE_UUID_FILE_NAME = ".dev_id.txt";
+    protected final String PREFS_DEVICE_ID       = "dev_id";
+    protected final String KEY                   = "cyril'98";
+    protected       UUID   uuid;
+
+    public DeviceUuidFactory(Context context) {
+        if (uuid == null) {
+            synchronized (DeviceUuidFactory.class) {
+                if (uuid == null) {
+                    final SharedPreferences prefs = context.getSharedPreferences(PREFS_FILE, 0);
+                    final String id = prefs.getString(PREFS_DEVICE_ID, null);
+                    if (id != null) {
+                        uuid = UUID.fromString(id);
+                    } else {
+                        if (recoverDeviceUuidFromSD() != null) {
+                            uuid = UUID.fromString(recoverDeviceUuidFromSD());
+                        } else {
+                            final String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+                            try {
+                                if (!"9774d56d682e549c".equals(androidId)) {
+                                    uuid = UUID.nameUUIDFromBytes(androidId.getBytes("utf8"));
+                                    try {
+                                        saveDeviceUuidToSD(EncryptUtils.encryptDES(uuid.toString(), KEY));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                } else {
+                                    uuid = UUID.randomUUID();
+                                    try {
+                                        saveDeviceUuidToSD(EncryptUtils.encryptDES(uuid.toString(), KEY));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } catch (UnsupportedEncodingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        prefs.edit().putString(PREFS_DEVICE_ID, uuid.toString()).commit();
+                    }
+                }
+            }
+        }
+    }
+
+    private String recoverDeviceUuidFromSD() {
+        try {
+            String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            File dir = new File(dirPath);
+            File uuidFile = new File(dir, DEVICE_UUID_FILE_NAME);
+            if (!dir.exists() || !uuidFile.exists()) {
+                return null;
+            }
+            FileReader fileReader = new FileReader(uuidFile);
+            StringBuilder sb = new StringBuilder();
+            char[] buffer = new char[100];
+            int readCount;
+            while ((readCount = fileReader.read(buffer)) > 0) {
+                sb.append(buffer, 0, readCount);
+            }
+            //通过UUID.fromString来检查uuid的格式正确�??
+            UUID uuid = UUID.fromString(EncryptUtils.decryptDES(sb.toString(), KEY));
+            return uuid.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void saveDeviceUuidToSD(String uuid) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File targetFile = new File(dirPath, DEVICE_UUID_FILE_NAME);
+        if (targetFile != null) {
+            if (targetFile.exists()) {
+
+            } else {
+                OutputStreamWriter osw;
+                try {
+                    osw = new OutputStreamWriter(new FileOutputStream(targetFile), "utf-8");
+                    try {
+                        osw.write(uuid);
+                        osw.flush();
+                        osw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+}
---
+++ usdk/android/demo.plugin.uuidkit/src/com/usdk/plugin/EncryptUtils.java
@@ -0,0 +1,41 @@
+package com.usdk.plugin;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptUtils {
+    public static String encryptDES(String encryptString, String encryptKey) throws Exception {
+        //返回实现指定转换的Cipher对象 "算法/模式/填充"
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        //创建�?个DESKeySpec对象，使�?8个字节的key作为DES密钥的内�?
+        DESKeySpec desKeySpec = new DESKeySpec(encryptKey.getBytes("UTF-8"));
+        //返回转换指定算法的秘密密钥的SecretKeyFactory对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        //根据提供的密钥生成SecretKey对象
+        SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
+        //使用iv中的字节作为iv来构造一个iv ParameterSpec对象。复制该缓冲区的内容来防止后续修�?
+        IvParameterSpec iv = new IvParameterSpec(encryptKey.getBytes());
+        //用密钥和�?组算法参数初始化�? Cipher；Cipher：加密�?�解密�?�密钥包装或密钥解包，具体取决于 opmode 的�?��??
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        //加密同时解码成字符串返回
+        return new String(BASE64.encode(cipher.doFinal(encryptString.getBytes("UTF-8"))));
+    }
+    public static String decryptDES(String decodeString, String decodeKey) throws Exception {
+        //使用指定密钥构�?�IV
+        IvParameterSpec iv = new IvParameterSpec(decodeKey.getBytes());
+        //根据给定的字节数组和指定算法构�?�一个密钥�??
+        SecretKeySpec skeySpec = new SecretKeySpec(decodeKey.getBytes(), "DES");
+        //返回实现指定转换�? Cipher 对象
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        //解密初始�?
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+        //解码返回
+        byte[] byteMi = BASE64.decode(decodeString.toCharArray());
+        byte decryptedData[] = cipher.doFinal(byteMi);
+        return new String(decryptedData);
+    }
+}
