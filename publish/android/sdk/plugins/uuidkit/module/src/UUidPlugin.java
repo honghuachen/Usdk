802ccbebda60dcdde57f5f0bc700cb16bc3707e4
chengdudahonghua <chengdudahonghua@outlook.com>
2020-05-27 11:37:08 +0800

    增加uuidkit插件

---
+++ publish/android/sdk/plugins/uuidkit/module/src/UUidPlugin.java
@@ -0,0 +1,21 @@
+package com.usdk.plugin.uuidkit;
+
+import com.usdk.sdk.UsdkBase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UUidPlugin extends UsdkBase{
+	DeviceUuidFactory uuidKit;
+	@Override
+	protected void OnCreate(Activity activity, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.OnCreate(activity, savedInstanceState);
+		uuidKit = new DeviceUuidFactory(activity);
+	}
+	
+	public String getUuid() {
+		return uuidKit.getUuid().toString();
+	}
+
+}
