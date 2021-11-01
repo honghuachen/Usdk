3ca6e7a5f4d94fc33e8ca643dc138584d5fc0a45
chengdudahonghua <chengdudahonghua@outlook.com>
2021-11-01 17:17:47 +0800

    增加回调调用示例

--- publish/android/sdk/platforms/none/module/src/PlatformProxy.java
+++ publish/android/sdk/platforms/none/module/src/PlatformProxy.java
@@ -1,12 +1,12 @@
 package com.usdk.plugin;
 
-import com.usdk.platform.adapter.PlatformProxyBase;
+import com.usdk.sdk.UsdkBase;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class PlatformProxy extends PlatformProxyBase {
+public class PlatformProxy extends UsdkBase {
 	private static String TAG = "PlatformProxy";
 	@Override
 	public void OnCreate(Activity activity, Bundle savedInstanceState) {
@@ -17,68 +17,33 @@ public class PlatformProxy extends PlatformProxyBase {
 	public void OnDestroy() {
 		Log.i(TAG, "OnDestroy ");
 	}
-
-	@Override
-	public void login(String custom_params_) {
-		Log.i(TAG, "login ");
-	}
-
-	@Override
-	public void logout(String custom_params_) {
-		Log.i(TAG, "logout ");
-	}
-
-	@Override
-	public void exit(String custom_params_) {
-		Log.i(TAG, "exit ");
-	}
-
-	@Override
-	public void pay(String pay_info_) {
-		Log.i(TAG, "pay ");
-	}
-
-	@Override
-	public void releaseSdkResource(String custom_params_) {
-		Log.i(TAG, "releaseSdkResource ");
-	}
-
-	@Override
-	public void switchAccount(String custom_params_) {
-		Log.i(TAG, "switchAccount ");
-	}
-
-	@Override
-	public void openUserCenter(String custom_params_) {
-		Log.i(TAG, "openUserCenter ");
-	}
-
-	@Override
-	public void openAppstoreComment(String appid) {
-		Log.i(TAG, "openAppstoreComment ");
-	}
 	
 	public void callPlugin(String a1,String a2,String a3,String a4) {
 		Log.i(TAG, "callPlugin ");
+		sendCallBack2Unity("callPlugin",a1);
 	}
 	
 	public int callPluginRInt(String a1,String a2,String a3,String a4) {
 		Log.i(TAG, "callPluginRInt ");
+		sendCallBack2Unity("callPluginRInt",a1);
 		return 1;
 	}
 	
 	public String callPluginRString(String a1,String a2,String a3,String a4) {
 		Log.i(TAG, "callPluginRString ");
+		sendCallBack2Unity("callPluginRString",a1);
 		return "callPluginRString";
 	}
 	
 	public boolean callPluginRBool(String a1,String a2,String a3,String a4) {
 		Log.i(TAG, "callPluginRBool ");
+		sendCallBack2Unity("callPluginRBool",a1);
 		return true;
 	}
 	
 	public float callPluginRFloat(String a1,String a2,String a3,String a4) {
 		Log.i(TAG, "callPluginRFloat ");
+		sendCallBack2Unity("callPluginRFloat",a1);
 		return 1.11f;
 	}
 }
